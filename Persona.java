f2f7dcc5558d51fc9d957132ee7fda4f76a6dc8c
ZzCry12 <dariorata@gmail.com>
2022-04-01 18:06:25 +0200

    Clase persona Editada

--- Persona.java
+++ Persona.java
@@ -1,10 +1,12 @@
 public class Persona {
    private String nombre;
+   private String poblacion;
    private int edad;
 
    public Persona(String nombre,int edad){
       this.nombre = nombre;
       this.edad = edad;	
+      this.poblacion = poblacion;
    }
 	
    public Persona() {
@@ -25,6 +27,13 @@ public class Persona {
    
    public int getEdad(){
       return edad;
+   }
+   
+   public void setPoblacion(String poblacion){
+   poblacion = poblacion;
+   }
+   public String getPoblacion(){
+   return poblacion;
    }
 
 }//fin Persona
